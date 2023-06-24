44655273d2f0ab29a5dabdc7dd2d6e0265cacab7
vidy <vidy.alfredo1@gmail.com>
2023-06-24 17:31:55 +0800

    add favorites page

--- src/modules/favorites/component/Favorites.tsx
+++ src/modules/favorites/component/Favorites.tsx
@@ -1,25 +1,23 @@
 import React, { useContext } from "react";
 import { FavoritesContext } from "..";
-import Movie from "../../movies/context/movieCtx";
-import { useFavoriteAPI } from "../context/favoritesAPI";
-
-const FavoriteItem: React.FC<{ movie: Movie }> = ({ movie }) => {
-  const { favorites, setFavorites } = useContext(FavoritesContext);
-  const { removeFromFavorite } = useFavoriteAPI(favorites, setFavorites);
-  return (
-    <div>
-      FavoriteItem
-      <button onClick={() => removeFromFavorite(movie)}>
-        remove From Favorite
-      </button>
-    </div>
-  );
-};
+// import Movie from "../../movies/context/movieCtx";
+// import { useFavoriteAPI } from "../context/favoritesAPI";
+import { MovieItem } from "../../movies/components/movies/movies";
+import { Header } from "../../../sharedComponents/Header/Header";
 
 export const Favorites: React.FC = () => {
   const { favorites } = useContext(FavoritesContext);
 
-  return Array.from(favorites).map(([, fmovie]) => {
-    return <FavoriteItem key={fmovie.id} movie={fmovie} />;
-  });
+  // favorites can reuse component from Movies
+
+  return (
+    <>
+      <Header title="My Favorites" />
+      <div className="favorite-page">
+        {Array.from(favorites).map(([, fmovie]) => {
+          return <MovieItem key={fmovie.id} movie={fmovie} />;
+        })}
+      </div>
+    </>
+  );
 };
