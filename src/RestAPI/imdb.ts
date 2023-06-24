51f08e720179c3f9ecc27075fa8c904c272db7e8
vidy <vidy.alfredo1@gmail.com>
2023-06-24 17:31:48 +0800

    imdbAPI to point to internet

--- src/RestAPI/imdb.ts
+++ src/RestAPI/imdb.ts
@@ -1,5 +1,3 @@
-import { MoviesMockData } from "../mockData/movies";
-
 const defaultLang = 'en';
 const apiKey = 'k_84kkttat'
 
@@ -7,7 +5,7 @@ export async function getMovies(lang: string = defaultLang){
     // const notification = useNotification();
 
     const url = `https://imdb-api.com/${lang}/API/Top250Movies/${apiKey}`;
-    return MoviesMockData
+
     return fetch(url).then(res => res.json())
     .catch(err => {
         console.log(err);
